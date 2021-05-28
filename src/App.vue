359c5523d34cc554b4fd3af197fe02d9fefe7833
Jacob Timms <jacob_tims@live.com>
2021-05-28 23:36:00 +0100

    "first commit"

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <head>
+      <link rel="stylesheet" href="./assets/css/mdb.min.css" type="text/css">
+    </head>
+    <side-nav></side-nav>
+    <div id="main">
+      <navBar></navBar>
+      <router-view :key="$route.path"></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navBar from './components/navBar'
+import SideNav from './components/sideNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    navBar,
+    SideNav,
+  },
 }
+
 </script>
 
 <style>
+/* Font Awespme */
+@import url('https://use.fontawesome.com/releases/v5.11.2/css/all.css');
+/* Google Fonts Roboto */
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
+/* Google Fonts Hind */
+@import url('https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500&display=swap');
+/* MBD */
+@import "./assets/css/mdb.min.css";
+
+html{
+  background-color: rgb(151, 146, 146);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Roboto', sans-serif;
 }
 </style>
